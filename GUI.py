e2c9a093e32ec5a77e7ab6f358385dfe322603e7
jonespaul327 <56886605+jonespaul327@users.noreply.github.com>
2020-07-14 07:48:28 -0700

    modified gui to display an image

--- GUI.py
+++ GUI.py
@@ -6,6 +6,12 @@ from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import *
 
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
 def select_path():
     global path
     curr_directory = os.getcwd()
@@ -13,20 +19,25 @@ def select_path():
     path.set(filename)
 
 
+def save_path():
+    curr_directory = os.getcwd()
+    filename = filedialog.askdirectory(initialdir=curr_directory, title="Select Folder")
+
+
 def completed_popup():
-    popup = tk.Tk()
-    popup.wm_title("Completed")
     messagebox.showinfo(title="Completed", message="Finished",)
+    button3 = tk.Button(root, text="Save", command=save_path)
+    button3.place(x=95, y=60)
 
 def dummy():
     main(path.get())
     print("Return Success")
     completed_popup()
 
-
 root = Tk()
 root.title('Efficiency')
-root.geometry('250x200')
+root.geometry('210x200')
+image = PhotoImage(file=resource_path("images.png"))
 
 path = StringVar()
 
@@ -36,8 +47,8 @@ label.place(x=0, y=5)
 entry =  tk.Entry(root, width=20, text=path)
 entry.place(x=52, y=7)
 
-button1 = tk.Button(root, text="select",  command=select_path)
-button1.place(x=180, y=0)
+button1 = tk.Button(root, image=image, width=20, height=20,  command=select_path)
+button1.place(x=180, y=3)
 
 button2 = tk.Button(root, text="GO",  command=dummy)
 button2.place(x=100, y=30)
