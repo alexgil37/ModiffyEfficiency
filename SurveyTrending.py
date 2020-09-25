c3a7bca38fc93b3aa55601579bd92fcca71d038c
jonespaul327 <56886605+jonespaul327@users.noreply.github.com>
2020-09-25 11:03:37 -0700

    Update SurveyTrending.py

--- SurveyTrending.py
+++ SurveyTrending.py
@@ -685,6 +685,9 @@ def main(path, savePath):
         x = 1
         for file in invalidFiles:
             FailedSheet.write(x, 0, file)
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 727d4ba... Merge branch 'master' of https://github.com/alexgil37/ModiffyEfficiency
 
     QCworkbook.close()
