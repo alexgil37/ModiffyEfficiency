c506ba691575e1ed02b1b3296350c51271cc691f
jonespaul327 <56886605+jonespaul327@users.noreply.github.com>
2020-10-19 16:06:44 -0700

    Update goblins.py

--- goblins.py
+++ goblins.py
@@ -1,5 +1,12 @@
-goblinNames = ["Greg", "Jim"]
-goblinMoney = [5, 10]
-
-response = input("Which goblin would you like to view ")
-print(goblinNames[int(response)] + " has " + str(goblinMoney[int(response)]) + " gold coins.")
+# -=[ goblins ]=-  6/97
+#              ,      ,
+#             /(.-""-.)\
+#         |\  \/      \/  /|
+#         | \ / =.  .= \ / |
+#         \( \   o\/o   / )/
+#          \_, '-/  \-' ,_/
+#            /   \__/   \
+#            \ \__/\__/ /
+#          ___\ \|--|/ /___
+#        /`    \      /    `\
+#   jgs /       '----'       \
